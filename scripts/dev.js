1d2c5a1bc551fe5ef15bb154c34e67d61f6d22f7
Nick Pisacane <pisacanen@gmail.com>
2016-11-10 16:29:20 -0800

    lint on rebuilt

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,13 +2,28 @@ const fs = require('fs')
 const path = require('path')
 const { execSync, spawn } = require('child_process')
 const debounce = require('lodash.debounce')
+const byline = require('byline')
+const map = require('map-stream')
 
 const srcPath = path.resolve(__dirname, path.join('..', 'src'))
 
+pipe(spawn('npm', ['run', 'serve-docs']))
 fs.watch(srcPath, debounce(() => {
   console.log('rebuilding docs...')
   execSync('npm run build-docs')
+
+  pipe(spawn('npm', ['run', 'lint']))
 }, 500))
 
-spawn('npm', ['run', 'serve-docs'])
-  .stdout.on('data', data => process.stdout.write(data))
+
+function pipe (src) {
+  noNpmErr(src.stdout).pipe(process.stdout)
+  noNpmErr(src.stderr).pipe(process.stderr)
+  return src
+}
+
+function noNpmErr (stream) {
+  return byline(stream).pipe(map((line, cb) =>
+   /^npm ERR\!/.test(line) ? cb(null, '') : cb(null, `${line}\n`)
+  ))
+}
